b4715333df2089f3a23999dc983d75f0228621dd
Erik Storrs <epstorrs@gmail.com>
2023-09-13 08:16:38 -0500

    Create cellranger_cmds.sh

---
+++ cellranger_cmds.sh
@@ -0,0 +1,3 @@
+cellranger-2.2.0/cellranger-cs/2.2.0/bin/count --id H_ZY-1105-07 --fastqs /diskmnt/Datasets/Pancreatic/H_ZY-1105-07/FASTQ --transcriptome /diskmnt/Datasets/Reference/CellRanger/refdata-cellranger-GRCh38-1.2.0 --expect-cells 6000 --jobmode=local --localcores=20 --localmem=60
+cellranger-2.2.0/cellranger-cs/2.2.0/bin/count --id H_ZY-1160-06 --fastqs /diskmnt/Datasets/Pancreatic/H_ZY-1160-06/FASTQ --transcriptome /diskmnt/Datasets/Reference/CellRanger/refdata-cellranger-GRCh38-1.2.0 --expect-cells 6000 --jobmode=local --localcores=20 --localmem=60
+
